8304dc3b156bbb301e48fb90cfb0ed046581aa13
Max R <maxx.ratto@gmail.com>
2019-11-26 20:31:31 -0700

    Removed unnecessary functions.

--- index.js
+++ index.js
@@ -93,71 +93,11 @@ function displayFilmDetails() {
     console.log('displayFilmDetails() ran');
 };
 
-function createAccount() {
-    //As a new user, I want to create an account.
-    console.log('createAccount() ran');
-};
-
-function signInToAccount() {
-    //As a user with an account, I want to login to my account.
-    console.log('signInToAccount() ran');
-};
-
-function addToWatchist() {
-    //As a user with an account, I want to add a film to my watchlist.
-    console.log('addToWatchlist() ran');
-};
-
-function removeFromWatchlist() {
-    //As a user with an account, I want to remove a film from my watchlist.
-    console.log('removeFromWatchlist() ran');
-};
-
-function displayWatchlist() {
-    //As a user with an account, I want to see my watchlist.
-    console.log('displayWatchlist() ran');
-};
-
-function editAccount() {
-    //As a user with an account, I want to edit my account.
-    console.log('editAccount() ran');
-};
-
-function deleteAccount() {
-    //As a user with an account, I want to delete my account.
-    console.log('deleteAccount() ran');
-};
-
-function seeAllUsers() {
-    //As an admin, I want to see all users.
-    console.log('seeAllUsers() ran');
-};
-
-function displayWatchlistReport() {
-    //As an admin, I want to see a report of films that have been added to watchlists and number of times.
-    console.log('displayWatchlistReport() ran');
-};
-
-function displayUserSearchHistory() {
-    //As an admin	I want to see a history of search queries for each user.
-    console.log('displayUserSearchHistory() ran');
-};
-
 $(function () {
     displaySearchResults();
     runSearch();
     setKeywords();
     displaySearchForm();
     displayFilmDetails();
-    createAccount();
-    signInToAccount();
-    addToWatchist();
-    removeFromWatchlist();
-    displayWatchlist();
-    editAccount();
-    deleteAccount();
-    seeAllUsers();
-    displayWatchlistReport();
-    displayUserSearchHistory();
     showMenu();
 });
