7c3b430aa0f8186011cfa5fe0033e14e8f43604a
Max R <maxx.ratto@gmail.com>
2019-11-30 14:04:09 -0700

    Fixed js bugs.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const SEARCH = {
     withKeywords: [],
     withoutKeywords: [],
-    primaryReleaseDateStart: '',
-    primaryReleaseDateEnd: '',
+    primaryReleaseDateStart: "",
+    primaryReleaseDateEnd: "",
     withPeople: [],
-    sortBy: 'popularity.desc',
+    sortBy: "popularity.desc",
     page: 1,
 };
 
-const tmdbToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTNlNmVlYjIwOGIxZWUxYWFiMDJjMjhiMjZjMDhiMSIsInN1YiI6IjVkZDI5Njg0NTdkMzc4MDAxM2RiNmVjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kXb3Vsx5XBDev3UHF7TnX8EDYPfvuKhNKSGMC2lkxzk';
-const apiKey = '813e6eeb208b1ee1aab02c28b26c08b1';
-const imdbApiKey = '0823bc3d86mshee07fc4e8741c97p13dedcjsn9e48b49e1470';
+const tmdbToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTNlNmVlYjIwOGIxZWUxYWFiMDJjMjhiMjZjMDhiMSIsInN1YiI6IjVkZDI5Njg0NTdkMzc4MDAxM2RiNmVjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kXb3Vsx5XBDev3UHF7TnX8EDYPfvuKhNKSGMC2lkxzk";
+const apiKey = "813e6eeb208b1ee1aab02c28b26c08b1";
+const imdbApiKey = "0823bc3d86mshee07fc4e8741c97p13dedcjsn9e48b49e1470";
 
 function generateSearchForm() {
     return `
@@ -78,27 +78,25 @@ function displaySearchForm() {
 };
 
 function fetchWithKeywordData(value) {
-    console.log(`fetchKeywordData() ran with and ${value}`);
     const options = {
         headers: new Headers({
             'Authorization': `Bearer ${tmdbToken}`
         })
     };
     fetch(`https://api.themoviedb.org/3/search/keyword?query=${value}`, options)
-        .then(response => response.json())
-        .then(responseJson => showWithKeywordResults(responseJson));
+        .then((response) => response.json())
+        .then((responseJson) => showWithKeywordResults(responseJson));
 };
 
 function fetchWithoutKeywordData(value) {
-    console.log(`fetchKeywordoutData() ran with and ${value}`);
     const options = {
         headers: new Headers({
             'Authorization': `Bearer ${tmdbToken}`
         })
     };
     fetch(`https://api.themoviedb.org/3/search/keyword?query=${value}`, options)
-        .then(response => response.json())
-        .then(responseJson => showWithoutKeywordResults(responseJson));
+        .then((response) => response.json())
+        .then((responseJson) => showWithoutKeywordResults(responseJson));
 };
 
 function fetchWithPeople(value) {
@@ -108,8 +106,8 @@ function fetchWithPeople(value) {
         })
     };
     fetch(`https://api.themoviedb.org/3/search/person?query=${value}`, options)
-        .then(response => response.json())
-        .then(responseJson => showPeopleAvailable(responseJson));
+        .then((response) => response.json())
+        .then((responseJson) => showPeopleAvailable(responseJson));
 };
 
 function fetchMasterSearch(masterSearchUrlString) {
@@ -119,8 +117,8 @@ function fetchMasterSearch(masterSearchUrlString) {
         })
     };
     fetch(`https://api.themoviedb.org/3/discover/movie${masterSearchUrlString}`, options)
-        .then(response => response.json())
-        .then(responseJson => fetchMovieDetails(responseJson));
+        .then((response) => response.json())
+        .then((responseJson) => fetchMovieDetails(responseJson));
 };
 
 function fetchMovieDetails(responseJson) {
@@ -141,21 +139,21 @@ function fetchMovieDetails(responseJson) {
 function fetchFilmDetailsFromImdb(imdbId) {
     const imdbOptions = {
         headers: new Headers({
-            'X-RapidAPI-Key': `0823bc3d86mshee07fc4e8741c97p13dedcjsn9e48b49e1470`
+            'X-RapidAPI-Key': "0823bc3d86mshee07fc4e8741c97p13dedcjsn9e48b49e1470"
         })
     };
     fetch(`https://movie-database-imdb-alternative.p.rapidapi.com/?i=${imdbId}`, imdbOptions)
-        .then(response => response.json())
-        .then(responseJson => displaySearchResults(responseJson));
+        .then((response) => response.json())
+        .then((responseJson) => displaySearchResults(responseJson));
 };
 
 function runKeywordSearch() {
-    $('main .form').on('click', '.with-keyword-submit-button', event => {
+    $('main .form').on('click', '.with-keyword-submit-button', (event) => {
         event.preventDefault();
         const value = $('.with-keyword-input').val();
         fetchWithKeywordData(value);
     });
-    $('main .form').on('click', '.without-keyword-submit-button', event => {
+    $('main .form').on('click', '.without-keyword-submit-button', (event) => {
         event.preventDefault();
         const value = $('.without-keyword-input').val();
         fetchWithoutKeywordData(value);
@@ -163,7 +161,7 @@ function runKeywordSearch() {
 };
 
 function runPeopleSearch() {
-    $('main .form').on('click', '.with-people-submit-button', event => {
+    $('main .form').on('click', '.with-people-submit-button', (event) => {
         event.preventDefault();
         const value = $('.with-people-input').val();
         fetchWithPeople(value);
@@ -187,7 +185,7 @@ function showWithoutKeywordResults(responseJson) {
     for (let i = 0; i < responseJson.results.length; i++) {
         $('.without-keyword-results').append(`
                 <input type="checkbox" name="without-keyword" value="${responseJson.results[i].id}">${responseJson.results[i].name}
-                `)
+                `);
     };
     $('.without-keyword-results').append(`</form>`);
 };
@@ -195,65 +193,59 @@ function showWithoutKeywordResults(responseJson) {
 function showPeopleAvailable(responseJson) {
     $('.people-results').append(`<form>`);
     for (let i = 0; i < responseJson.results.length; i++) {
-        $('.people-results').append(`<input type="checkbox" name="with-people" value="${responseJson.results[i].id}">${responseJson.results[i].name}`)
+        $('.people-results').append(`<input type="checkbox" name="with-people" value="${responseJson.results[i].id}">${responseJson.results[i].name}`);
     };
     $('.people-results').append(`</form>`);
 };
 
 function setWithKeywords() {
-    $('main .form').on('change', 'input[name=with-keyword]', event => {
+    $('main .form').on('change', 'input[name=with-keyword]', (event) => {
         const checkedBoxes = $('input[name=with-keyword]:checked');
         const keywordIds = [];
         for (let i = 0; i < checkedBoxes.length; i++) {
             keywordIds.push(checkedBoxes[i].value);
         };
         SEARCH.withKeywords = keywordIds;
-        console.log(SEARCH.withKeywords);
     });
 };
 
 function setWithoutKeywords() {
-    $('main .form').on('change', 'input[name=without-keyword]', event => {
+    $('main .form').on('change', 'input[name=without-keyword]', (event) => {
         const checkedBoxes = $('input[name=without-keyword]:checked');
         const keywordIds = [];
         for (let i = 0; i < checkedBoxes.length; i++) {
             keywordIds.push(checkedBoxes[i].value);
         };
         SEARCH.withoutKeywords = keywordIds;
-        console.log(SEARCH.withoutKeywords);
     });
 };
 
 function setReleaseYear() {
-    $('main .form').on('change', 'input[name=start-release-year]', event => {
+    $('main .form').on('change', 'input[name=start-release-year]', (event) => {
         const startReleaseYear = $('input[name=start-release-year]').val();
         SEARCH.primaryReleaseDateStart = startReleaseYear;
-        console.log(SEARCH.primaryReleaseDateStart);
     });
-    $('main .form').on('change', 'input[name=end-release-year]', event => {
+    $('main .form').on('change', 'input[name=end-release-year]', (event) => {
         const endReleaseYear = $('input[name=end-release-year]').val();
         SEARCH.primaryReleaseDateEnd = endReleaseYear;
-        console.log(SEARCH.primaryReleaseDateEnd);
     });
 };
 
 function setPeople() {
-    $('main .form').on('change', 'input[name=with-people]', event => {
+    $('main .form').on('change', 'input[name=with-people]', (event) => {
         const people = $('input[name=with-people]:checked');
         const peopleIds = [];
         for (let i = 0; i < people.length; i++) {
             peopleIds.push(people[i].value);
         };
         SEARCH.withPeople = peopleIds;
-        console.log(SEARCH.withPeople);
     });
 };
 
 function setSortBy() {
-    $('main .form').on('change', 'select[name=sort-by]', event => {
+    $('main .form').on('change', 'select[name=sort-by]', (event) => {
         const sortBy = $('select[name=sort-by]').val();
         SEARCH.sortBy = sortBy;
-        console.log(SEARCH.sortBy);
     })
 };
 
@@ -262,12 +254,11 @@ function generateMasterSearchUrlString() {
     const withoutKeywords = SEARCH.withoutKeywords.join("|");
     const withPeople = SEARCH.withPeople.join("|");
     const masterSearchUrlString = `?with_keywords=${withKeywords}&without_keywords=${withoutKeywords}&primary_release_date.gte=${SEARCH.primaryReleaseDateStart}&primary_release_date.lte=${SEARCH.primaryReleaseDateEnd}&with_people=${withPeople}&sort_by=${SEARCH.sortBy}&page=${SEARCH.page}`;
-    console.log(masterSearchUrlString);
     fetchMasterSearch(masterSearchUrlString);
 };
 
 function runMasterSearch() {
-    $('main .form').on('click', '.master-search-submit-button', event => {
+    $('main .form').on('click', '.master-search-submit-button', (event) => {
         event.preventDefault();
         generateMasterSearchUrlString();
     });
@@ -298,15 +289,14 @@ function displaySearchResults(responseJson) {
 };
 
 function startNewSearch() {
-    $('main .actions').on('click', 'button[name=start-new-search]', event => {
+    $('main .actions').on('click', 'button[name=start-new-search]', (event) => {
         location.reload();
     });
 };
 
 function loadMoreResults() {
-    $('main .actions').on('click', 'button[name=load-more-results]', event => {
+    $('main .actions').on('click', 'button[name=load-more-results]', (event) => {
         SEARCH.page += 1;
-        console.log(SEARCH.page)
         $('main .form').append(generateMasterSearchUrlString());
     });
 };
